aac10ae002cf434fed30441e83d71c7108dc1825
longnk1301 <khanhlong1315@gmail.com>
2021-12-08 10:54:30 +0700

    Refactor: migrate to typescript and install mui v5

---
+++ src/index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
+  document.querySelector('#root'),
+);
