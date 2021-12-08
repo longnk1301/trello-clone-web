8f5ca39aafa7df1e290213fefc972337722b3e41
longnk1301 <khanhlong1315@gmail.com>
2021-12-08 13:52:45 +0700

    Feat: add overflow for board

--- src/components/Board/index.tsx
+++ src/components/Board/index.tsx
@@ -6,7 +6,15 @@ import { BoardTitle, ImageStyled } from './styled';
 
 export const Board = () => {
   return (
-    <Box m={2} p={2} maxWidth={'350px'} minWidth={'350px'} borderRadius={2} bgcolor={colors.boardColor}>
+    <Box
+      m={2}
+      p={2}
+      maxWidth={'350px'}
+      minWidth={'350px'}
+      borderRadius={2}
+      bgcolor={colors.boardColor}
+      overflow={'auto'}
+    >
       <BoardTitle>Board</BoardTitle>
       <ImageStyled src={'https://www.fiditourjsc.com/wp-content/uploads/2020/10/featured-e1603256714543.jpg'} />
       <CardList />
